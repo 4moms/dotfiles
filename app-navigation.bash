0b17b76d01f65e77af62443f13c64f2835e2a63e
Benjamin R. Haskell <bhaskell@4moms.com>
2013-10-02 02:33:46 -0400

    Adds shorter names for some apps for app()

--- app-navigation.bash
+++ app-navigation.bash
@@ -1,5 +1,12 @@
 app() {
   local name=$1 env=$2 dev deploy
+
+  case "$name" in
+    a|aa) name=aardmark ;;
+    d) name=dragonfox ;;
+    dz) name=dragonzorro ;;
+  esac
+
   dev="$(dirname "$dotfiles")/$name"
   deploy="/var/www/${env:-production}/$name/current"
   if [ -e "$dev" ]
