59aa6a141c405519e36c0467c8e0ce285d446fa5
Benjamin R. Haskell <bhaskell@4moms.com>
2013-10-03 19:06:53 -0400

    Improves directory searching for app() function
    Now searches:
    
    $(dirname $dotfiles)/$app
    /var/www/$app/$env
    /var/www/$env/$app
    
    Priority is given to the homedir if $env is development or unspecified.
    
    /var/www is also searched with $env = production if $env is unspecified.

--- app-navigation.bash
+++ app-navigation.bash
@@ -1,5 +1,8 @@
 app() {
-  local name=$1 env=$2 dev deploy
+  local name=$1 env=$2 dir
+  local githome="$(dirname "$dotfiles")"
+  local deployhome=/var/www
+  local -a possible
 
   case "$name" in
     a|aa) name=aardmark ;;
@@ -7,12 +10,36 @@ app() {
     dz) name=dragonzorro ;;
   esac
 
-  dev="$(dirname "$dotfiles")/$name"
-  deploy="/var/www/${env:-production}/$name/current"
-  if [ -e "$dev" ]
-  then cd "$dev"
-  elif [ -e "$deploy" ]
-  then cd "$deploy"
-  else printf 'No directory found\n'
+  # if env is development or unspecified, try $githome and deployment dirs
+  if [[ "${env:-development}" == development ]] ; then
+    possible=(
+      "$githome"/"$name"
+      "$deployhome"/"$name"/development
+      "$deployhome"/development/"$name"
+    )
+    if [ -z "$env" ] ; then
+      possible+=(
+        "$deployhome"/"$name"/production
+        "$deployhome"/production/"$name"
+      )
+    fi
+  else
+    # otherwise, try deployment dirs
+    possible=(
+      "$deployhome"/"$name"/"$env"
+      "$deployhome"/"$env"/"$name"
+    )
   fi
+
+  # `cd` into the first possible directory that exists
+  for dir in "${possible[@]}" ; do
+    if [ -e "$dir" ] ; then
+      cd "$dir"
+      return 0
+    fi
+  done
+
+  # else print an error message
+  warn 'No directory found'
+  return 1
 }
