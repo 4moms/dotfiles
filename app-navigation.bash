9736f278f088c4d93fc402868946dc95cef01e09
Benjamin R. Haskell <bhaskell@4moms.com>
2013-10-03 19:17:37 -0400

    Renames var from $name to $app

--- app-navigation.bash
+++ app-navigation.bash
@@ -1,33 +1,33 @@
 app() {
-  local name=$1 env=$2 dir
+  local app=$1 env=$2 dir
   local githome="$(dirname "$dotfiles")"
   local deployhome=/var/www
   local -a possible
 
-  case "$name" in
-    a|aa) name=aardmark ;;
-    d) name=dragonfox ;;
-    dz) name=dragonzorro ;;
+  case "$app" in
+    a|aa) app=aardmark ;;
+    d) app=dragonfox ;;
+    dz) app=dragonzorro ;;
   esac
 
   # if env is development or unspecified, try $githome and deployment dirs
   if [[ "${env:-development}" == development ]] ; then
     possible=(
-      "$githome"/"$name"
-      "$deployhome"/"$name"/development
-      "$deployhome"/development/"$name"
+      "$githome"/"$app"
+      "$deployhome"/"$app"/development
+      "$deployhome"/development/"$app"
     )
     if [ -z "$env" ] ; then
       possible+=(
-        "$deployhome"/"$name"/production
-        "$deployhome"/production/"$name"
+        "$deployhome"/"$app"/production
+        "$deployhome"/production/"$app"
       )
     fi
   else
     # otherwise, try deployment dirs
     possible=(
-      "$deployhome"/"$name"/"$env"
-      "$deployhome"/"$env"/"$name"
+      "$deployhome"/"$app"/"$env"
+      "$deployhome"/"$env"/"$app"
     )
   fi
 
